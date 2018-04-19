a505f0765c56ea7cde0bb5e4e2a39863f039e5aa
wangenyong <wangenyong@gmail.com>
2018-04-19 14:37:45 +0800

    Save date.

--- pages/sportAdd/sportAdd.js
+++ pages/sportAdd/sportAdd.js
@@ -1,10 +1,13 @@
 // pages/sportNew/sportNew.js
-Page({
 
+var util = require('../../utils/util.js');
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    date: '',
     // 开始时间
     startTime: '19:50',
     // 结束时间
@@ -21,7 +24,17 @@ Page({
       '其他运动'
     ],
     // 运动数据
-    items: []
+    items: [],
+    loading: false
+  },
+
+  /**
+   * 训练日期设置
+   */
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
   },
 
   /**
@@ -61,10 +74,64 @@ Page({
   },
 
   /**
+   * 保存运动数据
+   */
+  bindSaveDataTap: function () {
+    this.setData({
+      loading: true
+    })
+    var time = this.calculateDuration();
+    var that = this;
+    wx.request({
+      url: 'http://localhost:3000/sports',
+      method: 'POST',
+      data: {
+        date: new Date(that.data.date),
+        category: that.data.sportArray[that.data.sportType],
+        duration: time.duration,
+        duration_suffix: time.duration_suffix,
+        datasets: that.data.items
+      },
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (err) {
+        console.log(err);
+      },
+      complete: function () {
+        that.setData({
+          loading: false
+        })
+      }
+    })
+  },
+
+  /**
+   * 计算时间间隔
+   */
+  calculateDuration: function () {
+    var startTime = new Date(this.data.date + " " + this.data.startTime);
+    var stopTime = new Date(this.data.date + " " + this.data.stopTime);
+    var duration = (stopTime.getTime() - startTime.getTime()) / 1000 / 60;
+    var duration_suffix = "分钟";
+    if (duration >= 60) {
+      duration = (duration / 60).toFixed(1);
+      duration_suffix = "小时";
+    }
+    return {
+      duration: duration,
+      duration_suffix: duration_suffix
+    }
+  },
+
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var date = util.formatDate(new Date());  
+    this.setData({
+      date: date
+    })
   },
 
   /**
